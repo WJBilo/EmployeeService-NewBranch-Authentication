0a4016c468da74dc0e8e11b59ac523159e1ebcdb
Wbilo <williambilojuul@gmail.com>
2018-11-02 15:47:21 +0100

    NyestVersIndenOpdat0211
    NyestVersIndenOpdat0211

--- EmployeeService/BasicAuthenticationAttribute.cs
+++ EmployeeService/BasicAuthenticationAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,30 +16,35 @@ namespace ElevService
 {
     public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
     {
+        // her overrider vi en method som er i AuthorizationFilterAttribute base klassen.
         public override void OnAuthorization(HttpActionContext actionContext)
         {
-         // var header = actionContext.Request.Headers.FirstOrDefault(h => h.Key.Equals("Authorization"));
-         
+            // The HTTP Authorization request header contains the credentials to authenticate a user agent with a server
+            // Hvis følgende == null så har brugeren ikke sendt de rigtige credentials. 
+            // Og så vil vi sende en unauthorized HTTP Response message. 
+
             if (actionContext.Request.Headers.Authorization == null)
             {
                 actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
             }
             else
             {
+                // authenticationToken kommer til at være base 64 encoded 
                 string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
-
+                // Her decoder vi authenticationToken 
+                // Får at få fat  i den decodede string skal vi benytte Encoding klassen og gøre følgende. 
                 string decodedAuthenticationToken = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
 
+                // Selve vores authenticationToken ser sådan her ud: username:password, når den er decoded
+                // Så vi skal splitte den ved kolon 
+                // Følgende komemer til at retunere et string array der kommer til at indeholde [0]brugernavnet og [1]passwordet. 
                 string[] usernamePasswordArray = decodedAuthenticationToken.Split(':');
                 string brugernavn = usernamePasswordArray[0];
                 string password = usernamePasswordArray[1];
 
-                if(ElevSecurity.Login(brugernavn, password))
-                {
-                    
-                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(brugernavn), null); 
-                }
-                else
+
+                // Her checker vi
+                if (!ElevSecurity.Login(brugernavn, password))
                 {
                     actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
                 }
@@ -47,3 +53,8 @@ namespace ElevService
         }
     }
 }
+
+
+
+
+
